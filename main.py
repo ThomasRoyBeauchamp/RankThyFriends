c89695eb55f414685f773f85ffad3e71400a32a1
thomasbeauchamp <thomasroybeauchamp@gmail.com>
2024-04-07 21:22:06 +0200

    added more logging/messaging

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     @bot.event
     async def on_message(message):
-        print("message identified")
+        game_log.info(f"received message from {message.author}")
         guild = bot.get_guild(GUILD)
         if message.author == bot.user:
             return
@@ -84,27 +84,34 @@ if __name__ == '__main__':
                     case 0:
                         await message.channel.send("Ranking received, thank you :) Where do you think you will"
                                                    f" fall in the overall ranking? (1-{game_instance.number_of_players})")
+                        game_log.info(f"Accepted ranking from {message.author.display_name}")
                     case 1:
                         await message.channel.send("Your order is not valid, please try again.")
+                        game_log.info(f"Rejected ranking from {message.author.display_name}")
                     case 2:
                         await message.channel.send("You are not currently in the game, please join with !join-game")
+                        game_log.info(f"Rejected ranking from {message.author.display_name}")
 
             elif not game_instance.has_submitted_current_round_guess(message.author.display_name):
                 match game_instance.submit_position_guess(msg, message.author.display_name):
                     case 0:
                         await message.channel.send(f"Guess received, thank you :) The round results will be posted "
                                                    f"in {guild.get_channel(game_instance.channel_id).name} shortly.")
+                        game_log.info(f"Accepted position guess from {message.author.display_name}")
                     case 1:
                         await message.channel.send(f"Invalid response. Please submit a single digit in the range"
                                                    f" 1-{game_instance.number_of_players}")
+                        game_log.info(f"Accepted position guess from {message.author.display_name}")
 
             else:
                 await message.channel.send(f"You have already submitted all required information for this"
                                            f" round, please wait for the next round to start.")
+                game_log.info(f"Rejected DM from {message.author.display_name}")
 
 
             if game_instance.received_all_responses and not game_instance.CALCULATING_ROUND_SCORES:
                 game_instance.CALCULATING_ROUND_SCORES = True
+                game_log.info("Calculating scores for round.")
                 game_instance.calculate_rankings()
                 output_channel = guild.get_channel(game_instance.channel_id)
 
@@ -116,6 +123,8 @@ if __name__ == '__main__':
                 final_ordering.sort(key=lambda x: x[1], reverse=True)
                 results_message += '\n'.join([f"{x[1]}: {x[0]}" for x in final_ordering]) + '\n\n'
 
+                game_log.info("Ranking for round:"+'|'.join([f"{x[1]}: {x[0]}" for x in final_ordering]))
+
                 results_message += 'You all thought you would be positioned as follows:\n\n'
                 results_message += '\n'.join([
                     f"{x[0]}: {x[1]}" for x in game_instance.current_round_position_guesses_entries
@@ -123,6 +132,9 @@ if __name__ == '__main__':
 
                 results_message += 'This means you each scored points as follows:\n\n'
                 results_message += '\n'.join(f"{x[0]}: {x[1]}" for x in game_instance.calculate_points().items()) + '.\n\n'
+
+                game_log.info("Points for round:" + '|'.join(
+                    f"{x[0]}: {x[1]}" for x in game_instance.calculate_points().items()))
 
                 results_message += 'The overall scores are as now follows:\n\n'
                 results_message += '\n'.join([
@@ -137,6 +149,7 @@ if __name__ == '__main__':
                     guild = bot.get_guild(GUILD)
                     output_channel = guild.get_channel(game_instance.channel_id)
                     await output_channel.send(f"The question this round is:\n\n {game_instance.current_question}")
+                    game_log.info(f"Round Question: {game_instance.current_question}")
 
                     players_in_game_list = '\n'.join([f"{x[0]+1}: {x[1]}" for x in enumerate(game_instance.player_names)])
                     for player in game_instance.players:
@@ -177,7 +190,8 @@ if __name__ == '__main__':
             game_log.info(f"added player {ctx.author.display_name} ({ctx.author.name}) to game")
             game_log.info(f"channel-token: {ctx.channel.id}")
             game_log.info(f'guild_id: {ctx.guild.id}')
-            message = f'added player {ctx.author.display_name} ({ctx.author.name}) to game'
+            message = (f'added player {ctx.author.display_name} ({ctx.author.name}) to game.'
+                       f'\nThere are {game_instance.number_of_players} players in the game ({game_instance.player_names}).')
         else:
             game_log.info(f"Wrong channel id.")
             message = f'This is not the active game channel (wrong channel id)'
@@ -197,6 +211,9 @@ if __name__ == '__main__':
 
     @bot.command(name='start-game')
     async def start_game(ctx: commands.Context):
+
+        game_log.info(f"Starting new game with players: {','.join(game_instance.player_names)}")
+
         game_instance.start_new_round()
         players_in_game_list = '\n'.join([f"{x[0] + 1}: {x[1]}" for x in enumerate(game_instance.player_names)])
         for player in game_instance.players:
@@ -211,8 +228,11 @@ if __name__ == '__main__':
     
         Enter your ranking in the format xyz, where x,y,z are either player ids and y can be 0 to indicate no preference for middle positions. 
                             """)
+
+        game_log.info(f"Round Question: {game_instance.current_question}")
         guild = bot.get_guild(GUILD)
         output_channel = guild.get_channel(game_instance.channel_id)
+
         await output_channel.send(f"The question this round is:\n\n {game_instance.current_question}")
 
         pass
